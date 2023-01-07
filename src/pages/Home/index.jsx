7db2658fc0784615ce839d8040fff52829fbcc4c
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-01-07 10:32:33 +0300

    select background fix

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,12 +12,8 @@ function Home() {
 }
 
 function Top() {
-	// Открытие и закрытие тулбара
-	const [isSelectOpen, setIsSelectOpen] = useState(false);
-	const handleSelectOpen = () => setIsSelectOpen(!isSelectOpen);
-
-	return (
-		<div className={styles.top}>
+	function Navigation() {
+		return (
 			<nav className={styles.navigation}>
 				<button
 					className={`${styles.navigation__item} ${styles["navigation__item-active"]}`}
@@ -32,7 +28,14 @@ function Top() {
 				<button className={styles.navigation__item}>Острые</button>
 				<button className={styles.navigation__item}>Закрытые</button>
 			</nav>
+		);
+	}
 
+	function Selector() {
+		// Открытие и закрытие тулбара
+		const [isSelectOpen, setIsSelectOpen] = useState(false);
+		const handleSelectOpen = () => setIsSelectOpen(!isSelectOpen);
+		return (
 			<div className={styles.select}>
 				<button
 					onClick={handleSelectOpen}
@@ -62,6 +65,13 @@ function Top() {
 					<button className={styles.select__item}>алфавиту</button>
 				</div>
 			</div>
+		);
+	}
+
+	return (
+		<div className={styles.top}>
+			<Navigation />
+			<Selector />
 		</div>
 	);
 }
@@ -80,18 +90,30 @@ function Content() {
 					<span className={styles.card__title}>Чизбургер-пицца</span>
 					<div className={styles.card__choice}>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>тонкое</li>
-							<li className={styles.choice__item}>традиционное</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								тонкое
+							</li>
+							<li className={styles.choice__item}>
+								традиционное
+							</li>
 						</ul>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>26 см.</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								26 см.
+							</li>
 							<li className={styles.choice__item}>30 см.</li>
 							<li className={styles.choice__item}>40 см.</li>
 						</ul>
 					</div>
 					<div className={styles.card__bottom}>
 						<span className={styles.card__price}>от 395 ₽</span>
-						<button className={`${styles.card__button} ${styles['card__button-secondary']}`}>
+						<button
+							className={`${styles.card__button} ${styles["card__button-secondary"]}`}
+						>
 							<svg
 								width="12"
 								height="12"
@@ -99,10 +121,13 @@ function Content() {
 								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
 							>
-								<path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="inherit"/>
+								<path
+									d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+									fill="inherit"
+								/>
 							</svg>
-                            Добавить
-                            <span>2</span>
+							Добавить
+							<span>2</span>
 						</button>
 					</div>
 				</div>
@@ -115,18 +140,30 @@ function Content() {
 					<span className={styles.card__title}>Чизбургер-пицца</span>
 					<div className={styles.card__choice}>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>тонкое</li>
-							<li className={styles.choice__item}>традиционное</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								тонкое
+							</li>
+							<li className={styles.choice__item}>
+								традиционное
+							</li>
 						</ul>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>26 см.</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								26 см.
+							</li>
 							<li className={styles.choice__item}>30 см.</li>
 							<li className={styles.choice__item}>40 см.</li>
 						</ul>
 					</div>
 					<div className={styles.card__bottom}>
 						<span className={styles.card__price}>от 395 ₽</span>
-						<button className={`${styles.card__button} ${styles['card__button-secondary']}`}>
+						<button
+							className={`${styles.card__button} ${styles["card__button-secondary"]}`}
+						>
 							<svg
 								width="12"
 								height="12"
@@ -134,10 +171,13 @@ function Content() {
 								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
 							>
-								<path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="inherit"/>
+								<path
+									d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+									fill="inherit"
+								/>
 							</svg>
-                            Добавить
-                            <span>2</span>
+							Добавить
+							<span>2</span>
 						</button>
 					</div>
 				</div>
@@ -150,18 +190,30 @@ function Content() {
 					<span className={styles.card__title}>Чизбургер-пицца</span>
 					<div className={styles.card__choice}>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>тонкое</li>
-							<li className={styles.choice__item}>традиционное</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								тонкое
+							</li>
+							<li className={styles.choice__item}>
+								традиционное
+							</li>
 						</ul>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>26 см.</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								26 см.
+							</li>
 							<li className={styles.choice__item}>30 см.</li>
 							<li className={styles.choice__item}>40 см.</li>
 						</ul>
 					</div>
 					<div className={styles.card__bottom}>
 						<span className={styles.card__price}>от 395 ₽</span>
-						<button className={`${styles.card__button} ${styles['card__button-secondary']}`}>
+						<button
+							className={`${styles.card__button} ${styles["card__button-secondary"]}`}
+						>
 							<svg
 								width="12"
 								height="12"
@@ -169,10 +221,13 @@ function Content() {
 								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
 							>
-								<path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="inherit"/>
+								<path
+									d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+									fill="inherit"
+								/>
 							</svg>
-                            Добавить
-                            <span>2</span>
+							Добавить
+							<span>2</span>
 						</button>
 					</div>
 				</div>
@@ -185,18 +240,30 @@ function Content() {
 					<span className={styles.card__title}>Чизбургер-пицца</span>
 					<div className={styles.card__choice}>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>тонкое</li>
-							<li className={styles.choice__item}>традиционное</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								тонкое
+							</li>
+							<li className={styles.choice__item}>
+								традиционное
+							</li>
 						</ul>
 						<ul className={styles.card__selector}>
-							<li className={`${styles.choice__item} ${styles['choice__item-active']}`}>26 см.</li>
+							<li
+								className={`${styles.choice__item} ${styles["choice__item-active"]}`}
+							>
+								26 см.
+							</li>
 							<li className={styles.choice__item}>30 см.</li>
 							<li className={styles.choice__item}>40 см.</li>
 						</ul>
 					</div>
 					<div className={styles.card__bottom}>
 						<span className={styles.card__price}>от 395 ₽</span>
-						<button className={`${styles.card__button} ${styles['card__button-secondary']}`}>
+						<button
+							className={`${styles.card__button} ${styles["card__button-secondary"]}`}
+						>
 							<svg
 								width="12"
 								height="12"
@@ -204,10 +271,13 @@ function Content() {
 								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
 							>
-								<path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="inherit"/>
+								<path
+									d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+									fill="inherit"
+								/>
 							</svg>
-                            Добавить
-                            <span>2</span>
+							Добавить
+							<span>2</span>
 						</button>
 					</div>
 				</div>
