da15e0e95d12a2d5433c39a3dbe794fe549a99f6
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-01-11 20:42:38 +0300

    skeleton with create-content-loader

---
+++ src/components/Card/Skeleton.jsx
@@ -0,0 +1,21 @@
+import ContentLoader from "react-content-loader";
+
+const Skeleton = (props) => (
+	<ContentLoader
+		speed={2}
+		width={282.5}
+		height={489.5}
+		viewBox="0 0 282.5 489.5"
+		backgroundColor="#d9d9d9"
+		foregroundColor="#e6e6e6"
+		{...props}
+	>
+		<circle cx="142" cy="136" r="135" />
+		<rect x="58" y="298" rx="4" ry="4" width="180" height="22" />
+		<rect x="4" y="342" rx="12" ry="12" width="277" height="94" />
+		<rect x="5" y="460" rx="6" ry="6" width="45" height="20" />
+		<rect x="141" y="452" rx="20" ry="20" width="140" height="38" />
+	</ContentLoader>
+);
+
+export default Skeleton;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,28 @@
 import { useEffect, useState } from "react";
 import Card from "../../components/Card";
+import Skeleton from "../../components/Card/Skeleton";
 import Top from "../../components/Top";
 import styles from "./index.module.styl";
 
 function Home() {
 	const [pizzas, setPizzas] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		fetch("https://63bd5257d660062388a18682.mockapi.io/items")
 			.then((res) => res.json())
-			.then((items) => setPizzas(items));
+			.then((items) => setPizzas(items))
+			.then(() => setIsLoading(false))
+			.catch((error) => {
+				alert("Ошибка \n " + error);
+				console.dir(error);
+				setIsLoading(false);
+			});
 	}, []);
 
 	const renderItems = () => {
+		if (isLoading)
+			return [...Array(8)].map((_, index) => <Skeleton key={index} />);
 		return pizzas.map((item) => <Card key={item.id} {...item} />);
 	};
 
