78494b25b3218fb5fa3cd15572de196adf2a893e
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-01-13 08:47:45 +0300

    scrolling to top when change location

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Ruotes, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import InfoBlock from "./components/InfoBlock";
+import ScrollToTop from "./components/ScrollToTop";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 
@@ -12,21 +13,21 @@ function App() {
 				<Header />
 
 				<main className="main">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/cart" element={<Cart />} />
-						<Route
-							path="*"
-							element={
-								<InfoBlock
-									title="404. Страница не найдена."
-									description=" К сожалению данная страница отсутствует в нашем интернет-магазине. Попросите с уважением"
-								/>
-							}
-						/>
-						Route
-					</Routes>
-					{/* <Cart /> */}
+					<ScrollToTop>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/cart" element={<Cart />} />
+							<Route
+								path="*"
+								element={
+									<InfoBlock
+										title="404. Страница не найдена."
+										description=" К сожалению данная страница отсутствует в нашем интернет-магазине. Попросите с уважением"
+									/>
+								}
+							/>
+						</Routes>
+					</ScrollToTop>
 				</main>
 			</div>
 		</div>
