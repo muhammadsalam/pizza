888f947bf64f25703f811fbc51f1b583909a15b6
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-01-17 17:18:08 +0300

    Обновление видимых items, когда category или selector изменяется

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,41 @@
 import { useEffect, useState } from "react";
+
+import styles from "./index.module.styl";
+
 import Card from "../../components/Card";
 import Skeleton from "../../components/Card/Skeleton";
-import Top from "../../components/Top";
-import styles from "./index.module.styl";
+import Category from "../../components/Category";
+import Selector from "../../components/Selector";
 
 function Home() {
 	const [pizzas, setPizzas] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
+	// Категория
+	const [categoryIndex, setCategoryIndex] = useState(0);
+	const onClickCategory = (index) => {
+		setCategoryIndex(index);
+	};
+
+	// Сортировка
+	const [sortType, setSortType] = useState({
+		name: "Популярности",
+		property: "rating",
+	});
+	const onClickSort = (index) => {
+		setSortType(index);
+	};
+
 	useEffect(() => {
-		fetch("https://63bd5257d660062388a18682.mockapi.io/items")
+		setIsLoading(true);
+
+		const category = categoryIndex > 0 ? `category=${categoryIndex}` : "";
+		const sort = sortType.property.replace("-", "");
+		const order = sortType.property.includes("-") ? "desc" : "asc";
+
+		fetch(
+			`https://63bd5257d660062388a18682.mockapi.io/items?${category}&sortBy=${sort}&order=${order}`
+		)
 			.then((res) => res.json())
 			.then((items) => setPizzas(items))
 			.then(() => setIsLoading(false))
@@ -18,27 +44,32 @@ function Home() {
 				console.dir(error);
 				setIsLoading(false);
 			});
-	}, []);
+	}, [categoryIndex, sortType]);
 
 	const renderItems = () => {
 		if (isLoading)
-			return [...Array(8)].map((_, index) => <Skeleton key={index} />);
+			return [...Array(4)].map((_, index) => (
+				<Skeleton className={styles.skeleton} key={index} />
+			));
 		return pizzas.map((item) => <Card key={item.id} {...item} />);
 	};
 
-	function Content() {
-		return (
-			<div className={styles.content}>
-				<h2 className={styles.content__title}>Все пиццы</h2>
-				<div className={styles.pizzas}>{renderItems()}</div>
-			</div>
-		);
-	}
-
 	return (
 		<>
-			<Top />
-			<Content />
+			<div className={styles.top}>
+				<Category
+					onClickCategory={onClickCategory}
+					categoryIndex={categoryIndex}
+				/>
+				<Selector sort={sortType} onChangeSort={onClickSort} />
+			</div>
+			<div className={styles.content}>
+				<h2 className={styles.content__title}>Все пиццы</h2>
+				<div className={styles.pizzas}>
+					{/* <Skeleton /> */}
+					{renderItems()}
+				</div>
+			</div>
 		</>
 	);
 }
